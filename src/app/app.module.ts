5c241338ac7568b4bda52c33527ae62f2c6ffc45
Hein Moe Thu <heinmoe32@gmail.com>
2019-12-22 12:02:34 -0800

    SideNav

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,40 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import {
+  MatToolbarModule,
+  MatSidenavModule,
+  MatButtonModule,
+  MatIconModule,
+  MatListModule,
+} from '@angular/material';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SideNavbarComponent } from './side-navbar/side-navbar.component';
+import { LayoutModule } from '@angular/cdk/layout';
+
+const material = [
+  MatToolbarModule,
+  MatSidenavModule,
+  MatButtonModule,
+  MatIconModule,
+  MatListModule,
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SideNavbarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      // { path: '', component: MainComponent },
+    ]),
+    BrowserAnimationsModule,
+    LayoutModule,
+    [material],
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/side-navbar/side-navbar.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { Observable } from 'rxjs';
+import { map, shareReplay } from 'rxjs/operators';
+
+@Component({
+  selector: 'app-side-navbar',
+  templateUrl: './side-navbar.component.html',
+  styleUrls: ['./side-navbar.component.css']
+})
+export class SideNavbarComponent {
+
+  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
+    .pipe(
+      map(result => result.matches),
+      shareReplay()
+    );
+
+  constructor(private breakpointObserver: BreakpointObserver) {}
+
+}
